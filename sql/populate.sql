82db4c20313d1260f1dd9fbbf0ce6e04ff4669c0
natehong <32336925+natehong@users.noreply.github.com>
2018-06-05 21:26:29 -0700

    added listings

--- sql/populate.sql
+++ sql/populate.sql
@@ -39,7 +39,7 @@ VALUES
 (2, 'Datsun', '280z', 1976),
 (3, 'Nissan', 'Altima', 2013),
 (4, 'Honda', 'Pilot', 2011),
-(5, 'Ram', '1500', 2016);
+(5, 'Dodge', 'Ram 1500', 2016);
 
 INSERT INTO books (item_id, title, author, genre)
 VALUES
@@ -68,6 +68,22 @@ VALUES
 INSERT INTO listings (price, title, description, post_date, exp_date, status, item_type, item, seller)
 VALUES
 (6000, '2001 4runner', 'ODO 240,000, excellent condition', CURRENT_DATE, '2018-07-01', 'active', 'auto', 1, 'nh'),
+(9000, '1976 280z', 'ODO ~160,000 runs and drives, good condition', CURRENT_DATE, '2018-07-10', 'active', 'auto', 2, 'nh'),
+(13500, 'Altima', '2013, excellent condition', CURRENT_DATE, '2018-06-21', 'active', 'auto', 3, 'nh'),
+(16500, 'Red Pilot', '2011, excellent condition, like new', CURRENT_DATE, '2018-06-22', 'active', 'auto', 4, 'nh'),
+(25000, 'RAM truck', '2016 new', CURRENT_DATE, '2018-06-27', 'active', 'auto', 5, 'nh'),
+(30, '2st Ed. The Cat in the Hat', 'Rare and 1 of 200', CURRENT_DATE, '2018-08-02', 'active', 'book', 6, 'dl'),
+(35, '3st Ed. Green Eggs and Ham', 'Rare and 1 of 300', CURRENT_DATE, '2018-08-03', 'active', 'book', 7, 'dl'),
+(45, '1st Ed. How the Grinch Stole Christmas', 'Rare and 1 of 100', CURRENT_DATE, '2018-08-05', 'active', 'book', 8, 'dl'),
 (25, '1st Ed. Fox in Sock', 'Rare and 1 of 100', CURRENT_DATE, '2018-08-01', 'active', 'book', 9, 'dl'),
-(700, 'Size 9 Jordan 1 Retro High Off-White', 'Flex or re-sell with this you hypebeast filth', CURRENT_DATE, '2018-09-01', 'active', 'shoe', 16, 'pm'),
-(2500, 'Minecraft on VHS', 'RIP', CURRENT_DATE, '2018-10-01', 'active', 'video game', 14, 'js');
+(40, '2st Ed. The Lorax', 'Rare and 1 of 200', CURRENT_DATE, '2018-08-04', 'active', 'book', 10, 'dl'),
+(25, 'GTA V on VHS', 'RIP', CURRENT_DATE, '2018-10-01', 'active', 'video game', 11, 'js'),
+(45, 'Legend of Zelda on VHS', 'RIP', CURRENT_DATE, '2018-10-02', 'active', 'video game', 12, 'js'),
+(40, 'Super Mario on VHS', 'RIP', CURRENT_DATE, '2018-10-03', 'active', 'video game', 13, 'js'),
+(2500, 'Minecraft on VHS', 'RIP', CURRENT_DATE, '2018-10-04', 'active', 'video game', 14, 'js'),
+(15, 'Cuphead on VHS', 'RIP', CURRENT_DATE, '2018-10-05', 'active', 'video game', 15, 'js'),
+(900, 'Jordan 1 Retro High Off-White', 'hypebeast filth', CURRENT_DATE, '2018-09-01', 'active', 'shoe', 16, 'pm'),
+(400, 'Yeezy 500 Blush', 'Kanye 2020', CURRENT_DATE, '2018-09-02', 'active', 'shoe', 17, 'pm'),
+(200, 'Thunder Spectra Black', 'New never worn', CURRENT_DATE, '2018-09-03', 'active', 'shoe', 18, 'pm'),
+(1000, 'Titan II Extra Butter', 'Issa shoe', CURRENT_DATE, '2018-09-04', 'active', 'shoe', 19, 'pm'),
+(60, 'Blakc Chuck Taylor', 'pleb shoes', CURRENT_DATE, '2018-09-05', 'active', 'shoe', 20, 'pm');
