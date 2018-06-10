a28f95a9f49ef83a0b8ef63cde9c51f9b47d801f
natehong <32336925+natehong@users.noreply.github.com>
2018-06-09 21:57:51 -0700

    generated more listings, sales, and items

--- sql/populate.sql
+++ sql/populate.sql
@@ -31,7 +31,27 @@ VALUES
 ('http://justfreshkicks.com/wp-content/uploads/2018/02/IMG_0022.jpg'),
 ('https://cdn.kixify.com/sites/default/files/imagecache/product_full/product/2018/03/25/p_22264631_112139166_10548426.jpg'),
 ('https://i.ebayimg.com/thumbs/images/g/g0YAAOSwuhFaJKM4/s-l225.jpg'),
-('https://m.media-amazon.com/images/I/81Q1cRhTHbL._SX480_.jpg');
+('https://m.media-amazon.com/images/I/81Q1cRhTHbL._SX480_.jpg'),
+('http://www.allfordmustangs.com/forums/attachments/2005-talk/6535d1110256200-pics-of-spoiler-delete-ricer.jpg'),
+('https://i.ytimg.com/vi/EhF5tPGvQ-A/maxresdefault.jpg'),
+('https://farm6.staticflickr.com/5493/14113969650_b2c33ef870_o.jpg'),
+('https://www.beamng.com/attachments/toyota-prius-camper-van-photo-gallery_4-jpg.69590/'),
+('http://www.audizine.com/gallery/data/500/a6_ricer.jpg'),
+('https://images.gr-assets.com/books/1276046901l/767680.jpg'),
+('https://images.gr-assets.com/books/1276046901l/767680.jpg'),
+('https://images.gr-assets.com/books/1276046901l/767680.jpg'),
+('https://images.gr-assets.com/books/1276046901l/767680.jpg'),
+('https://images.gr-assets.com/books/1276046901l/767680.jpg'),
+('https://images-na.ssl-images-amazon.com/images/I/41D5ZGDD2GL.jpg'),
+('https://steamcdn-a.akamaihd.net/steam/apps/702700/header.jpg?t=1528364944'),
+('https://mytintinnabulary.files.wordpress.com/2009/08/mii_channel.jpg'),
+('https://upload.wikimedia.org/wikipedia/en/thumb/d/dd/Fortnite_cover_art%28PC%29.jpg/220px-Fortnite_cover_art%28PC%29.jpg'),
+('https://images-na.ssl-images-amazon.com/images/I/51vbRQnPLVL.jpg'),
+('http://cdn.shopify.com/s/files/1/1374/2665/products/Screenshot_2_a07f74fa-a7ae-4218-a58d-8c70e2a19fab_grande.png?v=1516732988'),
+('http://cdn.shopify.com/s/files/1/1374/2665/products/Screenshot_2_grande.png?v=1516732500'),
+('https://www.shoecarnival.com/dw/image/v2/BBSZ_PRD/on/demandware.static/-/Sites-scvl-master-catalog/default/dwde9f44d5/91392_175409_1.jpg?sw=1694&sh=1999&sm=fit'),
+('https://richmedia.channeladvisor.com/ImageDelivery/imageService?profileId=12013292&itemId=1076344&swatchId=1076344-2-AS&recipeName=500'),
+('https://cdn.shopify.com/s/files/1/1374/2665/products/Screenshot_71_grande.png?v=1516732902');
 
 INSERT INTO autos (item_id, make, model, year)
 VALUES
@@ -39,7 +59,12 @@ VALUES
 (2, 'Datsun', '280z', 1976),
 (3, 'Nissan', 'Altima', 2013),
 (4, 'Honda', 'Pilot', 2011),
-(5, 'Dodge', 'Ram 1500', 2016);
+(5, 'Dodge', 'Ram 1500', 2016),
+(21, 'Toyota', 'Camry', 2003),
+(22, 'Honda', 'Civic', 2016),
+(23, 'Ford', 'Fiesta', 2012),
+(24, 'Toyota', 'Prius', 2015),
+(25, 'Audi', 'A4', 2008);
 
 INSERT INTO books (item_id, title, author, genre)
 VALUES
@@ -47,7 +72,12 @@ VALUES
 (7, 'Green Eggs and Ham', 'Dr. Seuss', 'anime'),
 (8, 'How the Grinch Stole Christmas', 'Dr. Seuss', 'christmas'),
 (9, 'Fox in Socks', 'Dr. Seuss', 'drama'),
-(10, 'The Lorax', 'Dr. Seuss', 'action');
+(10, 'The Lorax', 'Dr. Seuss', 'action'),
+(26, 'If You Give a Mouse a Cookie', 'Laura Joffe Numeroff', 'anime'),
+(27, 'If You Give a Mouse a Car', 'Laura Joffe Numeroff', 'anime'),
+(28, 'If You Give a Mouse a Taco', 'Laura Joffe Numeroff', 'anime'),
+(29, 'If You Give a Mouse a Hunny', 'Laura Joffe Numeroff', 'anime'),
+(30, 'If You Give a Mouse a Plane', 'Laura Joffe Numeroff', 'anime');
 
 INSERT INTO video_games (item_id, title, rating, genre)
 VALUES
@@ -55,7 +85,12 @@ VALUES
 (12, 'The Legend of Zelda', 9, 'adventure'),
 (13, 'Super Mario Odyssey', 8, 'platform'),
 (14, 'Minecraft', 10, 'sandbox'),
-(15, 'Cuphead', 8, 'Shoot em up');
+(15, 'Cuphead', 8, 'Shoot em up'),
+(31, 'Kia', 9, 'adventure'),
+(32, 'Bomberman', 8, 'arcade'),
+(33, 'Mii Channel', 10, 'sandbox'),
+(34, 'Fortnite', 0, 'battle royale'),
+(35, 'Megaman', 7, 'classic');
 
 INSERT INTO shoes (item_id, brand, model, size)
 VALUES
@@ -63,7 +98,12 @@ VALUES
 (17, 'Adidas', 'Yeezy 500 Blush', 8),
 (18, 'Puma', 'Thunder Spectra Black', 10),
 (19, 'Diadora', 'Titan II Extra Butter', 11),
-(20, 'Converse', 'Chuck Taylor', 9);
+(20, 'Converse', 'Chuck Taylor', 9),
+(36, 'Skechers', '1 X Supreme', 44),
+(37, 'Skechers', '2 X Off-White', 44),
+(38, 'Skechers', '3 X Gucci', 44),
+(39, 'Skechers', '4 X Prada', 44),
+(40, 'Skechers', '5 X Ralph Lauren', 44);
 
 INSERT INTO listings (price, title, description, post_date, exp_date, status, item_type, item, seller)
 VALUES
@@ -86,4 +126,68 @@ VALUES
 (400, 'Yeezy 500 Blush', 'Kanye 2020', CURRENT_TIMESTAMP, '2018-09-02 00:00:00', 'active', 'Shoe', 17, 'pm'),
 (200, 'Thunder Spectra Black', 'New never worn', CURRENT_TIMESTAMP, '2018-09-03 00:00:00', 'active', 'Shoe', 18, 'pm'),
 (300, 'Titan II Extra Butter', 'Issa shoe', CURRENT_TIMESTAMP, '2018-09-04 00:00:00', 'active', 'Shoe', 19, 'pm'),
-(60, 'Black Chuck Taylor', 'pleb shoes', CURRENT_TIMESTAMP, '2018-09-05 00:00:00', 'active', 'Shoe', 20, 'pm');
+(60, 'Black Chuck Taylor', 'pleb shoes', CURRENT_TIMESTAMP, '2018-09-05 00:00:00', 'active', 'Shoe', 20, 'pm'),
+(2500, '2003 Toyota Camry Sedan LE Sedan 4D', 'Not a stolen car, come buy pls', CURRENT_TIMESTAMP, '2018-10-01 00:00:00', 'active', 'Auto', 21, 'pm'),
+(16650, '2016 Honda Civic LX low miles, clean title', 'Almost new, still under warrenty, come take a look', CURRENT_TIMESTAMP, '2018-10-01 00:00:00', 'active', 'Auto', 22, 'pm'),
+(5500, '2012 Ford Fiesta SE Hatchback', '2012 Ford Fiesta very good condition, reliable & well maintained.', CURRENT_TIMESTAMP, '2018-10-01 00:00:00', 'active', 'Auto', 23, 'pm'),
+(12900, '2015 Toyota Prius', 'TOYOTA PRIUS 2015 CLEAN TITLE 54 thousand miles', CURRENT_TIMESTAMP, '2018-10-01 00:00:00', 'active', 'Auto', 24, 'pm'),
+(5400, '2008 Audi A4 2.0 Turbo', 'Smog done a couple days ago. Transmission smooth. Suspension nice and firm', CURRENT_TIMESTAMP, '2018-10-01 00:00:00', 'active', 'Auto', 25, 'pm'),
+(44, '1st Ed. If You Give a Mouse a Cookie', 'Sweet Jesus...Rare and 1 of 100', CURRENT_TIMESTAMP, '2018-10-01 00:00:00', 'active', 'Book', 26, 'js'),
+(55, '2st Ed. If You Give a Mouse a Car', 'Sweet Jesus...Rare and 1 of 200', CURRENT_TIMESTAMP, '2018-10-01 00:00:00', 'active', 'Book', 27, 'js'),
+(66, '3st Ed. If You Give a Mouse a Taco', 'Sweet Jesus...Rare and 1 of 300', CURRENT_TIMESTAMP, '2018-10-01 00:00:00', 'active', 'Book', 28, 'js'),
+(77, '4st Ed. If You Give a Mouse a Hunny', 'Sweet Jesus...Rare and 1 of 400', CURRENT_TIMESTAMP, '2018-10-01 00:00:00', 'active', 'Book', 29, 'js'),
+(88, '5st Ed. If You Give a Mouse a Plane', 'Sweet Jesus...Rare and 1 of 500', CURRENT_TIMESTAMP, '2018-10-01 00:00:00', 'active', 'Book', 30, 'js'),
+(22, 'KIA on CASSETTE', 'RIP', CURRENT_TIMESTAMP, '2018-10-01 00:00:00', 'active', 'Video Game', 31, 'dl'),
+(33, 'Bomberman on CASSETTE', 'RIP', CURRENT_TIMESTAMP, '2018-10-01 00:00:00', 'active', 'Video Game', 32, 'dl'),
+(44, 'Mii Channel on CASSETTE', 'RIP', CURRENT_TIMESTAMP, '2018-10-01 00:00:00', 'active', 'Video Game', 33, 'dl'),
+(55, 'Fortnite on CASSETTE', 'RIP', CURRENT_TIMESTAMP, '2018-10-01 00:00:00', 'active', 'Video Game', 34, 'dl'),
+(66, 'Megaman on CASSETTE', 'RIP', CURRENT_TIMESTAMP, '2018-10-01 00:00:00', 'active', 'Video Game', 35, 'dl'),
+(6969, 'Skechers 1 X Supreme', 'hypebeast filth god tier stuff right here', CURRENT_TIMESTAMP, '2018-10-01 00:00:00', 'active', 'Shoe', 36, 'nh'),
+(6969, 'Skechers 2 X Off-White', 'hypebeast filth god tier stuff right here', CURRENT_TIMESTAMP, '2018-10-01 00:00:00', 'active', 'Shoe', 37, 'nh'),
+(6969, 'Skechers 3 X Gucci', 'hypebeast filth god tier stuff right here', CURRENT_TIMESTAMP, '2018-10-01 00:00:00', 'active', 'Shoe', 38, 'nh'),
+(6969, 'Skechers 4 X Prada', 'hypebeast filth god tier stuff right here', CURRENT_TIMESTAMP, '2018-10-01 00:00:00', 'active', 'Shoe', 39, 'nh'),
+(6969, 'Skechers 5 X Ralph Lauren', 'hypebeast filth god tier stuff right here', CURRENT_TIMESTAMP, '2018-10-01 00:00:00', 'active', 'Shoe', 40, 'nh'),
+-- sold / expired listings
+(2500, '2003 Toyota Camry Sedan LE Sedan 4D', 'Not a stolen car, come buy pls', '2018-06-01 00:00:00', CURRENT_TIMESTAMP, 'active', 'Auto', 21, 'pm'),
+(16650, '2016 Honda Civic LX low miles, clean title', 'Almost new, still under warrenty, come take a look', '2018-06-01 00:00:00', CURRENT_TIMESTAMP, 'active', 'Auto', 22, 'pm'),
+(5500, '2012 Ford Fiesta SE Hatchback', '2012 Ford Fiesta very good condition, reliable & well maintained.', '2018-06-01 00:00:00', CURRENT_TIMESTAMP, 'active', 'Auto', 23, 'pm'),
+(12900, '2015 Toyota Prius', 'TOYOTA PRIUS 2015 CLEAN TITLE 54 thousand miles', '2018-06-01 00:00:00', CURRENT_TIMESTAMP, 'active', 'Auto', 24, 'pm'),
+(5400, '2008 Audi A4 2.0 Turbo', 'Smog done a couple days ago. Transmission smooth. Suspension nice and firm', '2018-06-01 00:00:00', CURRENT_TIMESTAMP, 'active', 'Auto', 25, 'pm'),
+(44, '1st Ed. If You Give a Mouse a Cookie', 'Sweet Jesus...Rare and 1 of 100', '2018-06-01 00:00:00', CURRENT_TIMESTAMP, 'active', 'Book', 26, 'js'),
+(55, '2st Ed. If You Give a Mouse a Car', 'Sweet Jesus...Rare and 1 of 200', '2018-06-01 00:00:00', CURRENT_TIMESTAMP, 'active', 'Book', 27, 'js'),
+(66, '3st Ed. If You Give a Mouse a Taco', 'Sweet Jesus...Rare and 1 of 300', '2018-06-01 00:00:00', CURRENT_TIMESTAMP, 'active', 'Book', 28, 'js'),
+(77, '4st Ed. If You Give a Mouse a Hunny', 'Sweet Jesus...Rare and 1 of 400', '2018-06-01 00:00:00', CURRENT_TIMESTAMP, 'active', 'Book', 29, 'js'),
+(88, '5st Ed. If You Give a Mouse a Plane', 'Sweet Jesus...Rare and 1 of 500', '2018-06-01 00:00:00', CURRENT_TIMESTAMP, 'active', 'Book', 30, 'js'),
+(22, 'KIA on CASSETTE', 'RIP', '2018-05-02 00:00:00', CURRENT_TIMESTAMP, 'active', 'Video Game', 31, 'dl'),
+(33, 'Bomberman on CASSETTE', 'RIP', '2018-05-02 00:00:00', CURRENT_TIMESTAMP, 'active', 'Video Game', 32, 'dl'),
+(44, 'Mii Channel on CASSETTE', 'RIP', '2018-05-02 00:00:00', CURRENT_TIMESTAMP, 'active', 'Video Game', 33, 'dl'),
+(55, 'Fortnite on CASSETTE', 'RIP', '2018-05-02 00:00:00', CURRENT_TIMESTAMP, 'active', 'Video Game', 34, 'dl'),
+(66, 'Megaman on CASSETTE', 'RIP', '2018-05-02 00:00:00', CURRENT_TIMESTAMP, 'active', 'Video Game', 35, 'dl'),
+(6969, 'Skechers 1 X Supreme', 'hypebeast filth god tier stuff right here', '2018-05-21 00:00:00', CURRENT_TIMESTAMP, 'active', 'Shoe', 36, 'nh'),
+(6969, 'Skechers 1 X Off-White', 'hypebeast filth god tier stuff right here', '2018-05-21 00:00:00', CURRENT_TIMESTAMP, 'active', 'Shoe', 37, 'nh'),
+(6969, 'Skechers 1 X Gucci', 'hypebeast filth god tier stuff right here', '2018-05-21 00:00:00', CURRENT_TIMESTAMP, 'active', 'Shoe', 38, 'nh'),
+(6969, 'Skechers 1 X Prada', 'hypebeast filth god tier stuff right here', '2018-05-21 00:00:00', CURRENT_TIMESTAMP, 'active', 'Shoe', 39, 'nh'),
+(6969, 'Skechers 1 X Ralph Lauren', 'hypebeast filth god tier stuff right here', '2018-05-21 00:00:00', CURRENT_TIMESTAMP, 'active', 'Shoe', 40, 'nh');
+
+INSERT INTO sales (listing, buyer, sale_date)
+VALUES
+(41, 'js', CURRENT_TIMESTAMP),
+(42, 'nh', CURRENT_TIMESTAMP),
+(43, 'dl', CURRENT_TIMESTAMP),
+(44, 'pm', CURRENT_TIMESTAMP),
+(45, 'js', CURRENT_TIMESTAMP),
+(46, 'nh', CURRENT_TIMESTAMP),
+(47, 'dl', CURRENT_TIMESTAMP),
+(48, 'pm', CURRENT_TIMESTAMP),
+(49, 'js', CURRENT_TIMESTAMP),
+(50, 'nh', CURRENT_TIMESTAMP),
+(51, 'dl', CURRENT_TIMESTAMP),
+(52, 'pm', CURRENT_TIMESTAMP),
+(53, 'js', CURRENT_TIMESTAMP),
+(54, 'nh', CURRENT_TIMESTAMP),
+(55, 'dl', CURRENT_TIMESTAMP),
+(56, 'pm', CURRENT_TIMESTAMP),
+(57, 'js', CURRENT_TIMESTAMP),
+(58, 'nh', CURRENT_TIMESTAMP),
+(59, 'dl', CURRENT_TIMESTAMP),
+(60, 'pm', CURRENT_TIMESTAMP);
